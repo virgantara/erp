ef02702d8d5fb1c35e3a5e6835e854d16fa03e76
virgantara <vinux.edu@gmail.com>
2018-01-12 13:12:50 +0700

    init skt

---
+++ _protected/config/web.php
@@ -0,0 +1,117 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'basic',
+    'name' => 'SKT | Sistem Keuangan Terintegrasi',
+    'language' => 'en',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log', 'app\components\Aliases'],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'bismillah',
+        ],
+        // you can set your theme here - template comes with: 'light' and 'dark'
+        'view' => [
+            'theme' => [
+                'pathMap' => ['@app/views' => '@webroot/themes/blue/views'],
+                'baseUrl' => '@web/themes/blue',
+            ],
+        ],
+        'assetManager' => [
+            'bundles' => [
+                // we will use bootstrap css from our theme
+                'yii\bootstrap\BootstrapAsset' => [
+                    'css' => [], // do not use yii default one
+                ],
+            ],
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '<alias:\w+>' => 'site/<alias>',
+            ],
+        ],
+        'user' => [
+            'identityClass' => 'app\models\UserIdentity',
+            'enableAutoLogin' => true,
+        ],
+        'session' => [
+            'class' => 'app\components\SafeSession',
+            'savePath' => '@app/runtime/session'
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'cache' => 'cache',
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. 
+            // You have to set 'useFileTransport' to false and configure a transport for the mailer to send real emails.
+            'useFileTransport' => true,
+
+            // 'transport' => [
+            //     'class' => 'Swift_SmtpTransport',
+            //     'host' => 'smtp.unida.gontor.ac.id',
+            //     'username' => 'pptik@unida.gontor.ac.id',
+            //     'password' => 'p$p$t1k4',
+            //     'port' => '587',
+            //     'encryption' => 'tls',                                  
+            // ], 
+            // 'transport' => [
+            //     'class' => 'Swift_SmtpTransport',
+            //     'host' => 'smtp.gmail.com',
+            //     'username' => 'vinux.edu@gmail.com',
+            //     'password' => 'dcrblclhqzcyxnso',
+            //     'port' => '587',
+            //     'encryption' => 'tls',                                  
+            // ], 
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/translations',
+                    'sourceLanguage' => 'en',
+                ],
+                'yii' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/translations',
+                    'sourceLanguage' => 'en'
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = ['class' => 'yii\debug\Module'];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = ['class' => 'yii\gii\Module'];
+}
+
+return $config;
