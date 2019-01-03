52cce7fec41f4e0270b97122de360aa3a64745e1
oddyvirgantara <oddy@unida.gontor.ac.id>
2019-01-03 11:49:46 +0700

    fix hitung kartu stok

--- _protected/controllers/RequestOrderController.php
+++ _protected/controllers/RequestOrderController.php
@@ -221,7 +221,7 @@ class RequestOrderController extends Controller
                         'tanggal' => $model->tanggal_penyetujuan,
                         'departemen_id' => $item->ro->departemen_id_to,
                         'stok_id' => $item->stok_id,
-                        'keterangan' => $item->ro->getNamaDeptTujuan(),
+                        'keterangan' => $item->ro->getNamaDeptAsal(),
                     ];
 
                     \app\models\KartuStok::createKartuStok($params);
@@ -233,7 +233,7 @@ class RequestOrderController extends Controller
                         'tanggal' => $model->tanggal_penyetujuan,
                         'departemen_id' => $item->ro->departemen_id,
                         'stok_id' => $item->stok_id,
-                        'keterangan' => $item->ro->getNamaDeptAsal(),
+                        'keterangan' => $item->ro->getNamaDeptTujuan(),
                     ];
 
                     \app\models\KartuStok::createKartuStok($params);
