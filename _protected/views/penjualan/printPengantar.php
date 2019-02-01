b51cffdaf707be991fe05d406d82593f6696d0eb
oddyvirgantara <oddy@unida.gontor.ac.id>
2019-02-01 12:59:57 +0700

    fix pisah cart racikan

--- _protected/views/penjualan/printPengantar.php
+++ _protected/views/penjualan/printPengantar.php
@@ -1,22 +1,24 @@
 <?php
 use yii\helpers\Url;
 use yii\helpers\Html;
-$fontfamily = 'Arial';
+$fontfamily = 'Times';
+$fontSize = '20px';
+$fontSizeBawah = '18px';
 ?>
 <table width="100%" style="height: 1px;margin: 0px">
     <tr>
         <td width="10%"></td>
         <td width="80%" style="text-align: center">
-            <strong style="font-size: 11px;font-family: <?=$fontfamily;?>">RSUD KABUPATEN KEDIRI</strong><br>
-            <span style="font-size:8px;font-family: <?=$fontfamily;?>">Jl. PAHLAWAN KUSUMA BANGSA NO 1 TLP (0354) 391718, 391169, 394956 FAX. 391833<BR>
+            <strong style="font-size: 14px;font-family: <?=$fontfamily;?>">RSUD KABUPATEN KEDIRI</strong><br>
+            <span style="font-size:10px;font-family: <?=$fontfamily;?>">Jl. PAHLAWAN KUSUMA BANGSA NO 1 TLP (0354) 391718, 391169, 394956 FAX. 391833<BR>
             PARE KEDIRI (64213) email : rsud.pare@kedirikab.go.id</span>
         </td>
         <td width="10%"></td>
     </tr>
 </table>
 <hr style="height: 1px;margin: 0px">
-<div style="text-align: center;margin: 0px;font-size:11px;font-family: <?=$fontfamily;?>">SURAT PENGANTAR BAYAR OBAT</div>
-<table style="border: 1px solid;margin-bottom: 3px;font-size: 12px;font-family: <?=$fontfamily;?>">
+<div style="text-align: center;margin: 0px;font-size:12px;font-family: <?=$fontfamily;?>">SURAT PENGANTAR BAYAR OBAT</div>
+<table style="border: 1px solid;margin-bottom: 3px;font-size: <?=$fontSizeBawah;?>;font-family: <?=$fontfamily;?>">
     <tr>
         <td style="width: 100px">No Resep</td>
         <td  style="width: 20px">:</td>
@@ -35,7 +37,7 @@ $fontfamily = 'Arial';
     </tr>
      
 </table>
-<table style="border: 1px solid;font-size: 12px;font-family: <?=$fontfamily;?>">
+<table style="border: 1px solid;font-size: <?=$fontSizeBawah;?>;font-family: <?=$fontfamily;?>">
     
      <tr>
         <td style="width: 100px" >No RM</td>
@@ -43,7 +45,7 @@ $fontfamily = 'Arial';
         <td style="width: 250px"><?=$model->customer_id;?></td>
     </tr>
      <tr>
-        <td >Jenis Pasien</td>
+        <td >Jenis Px</td>
         <td>:</td>
         <td><?=$reg->kodeGol->NamaGol;?></td>
     </tr>
@@ -71,7 +73,7 @@ $fontfamily = 'Arial';
 <table width="100%">
     <tr>
         
-        <td width="100%" style="text-align: center;font-size:9px;font-family: <?=$fontfamily;?>">
+        <td width="100%" style="text-align: center;font-size:14px;font-family: <?=$fontfamily;?>">
             <br><br>
             Pare, <?=date('d-m-Y');?>
             <br>
