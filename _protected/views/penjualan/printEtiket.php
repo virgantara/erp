ec0cdeb7ba833f4df53880a771c1feb9f8d6a9a8
Oddy Virgantara Putra <oddy@unida.gontor.ac.id>
2019-04-01 09:45:02 +0700

    fix font size

--- _protected/views/penjualan/printEtiket.php
+++ _protected/views/penjualan/printEtiket.php
@@ -5,38 +5,38 @@ $fontfamily = 'Times';
 $fontSize = '20px';
 $fontSizeBawah = '12px';
 ?>
-<table width="100%" style="margin: 0px" cellpadding="0">
+<table width="100%" style="margin: 0px" cellpadding="0" >
     <tr>
-        <td width="10%"></td>
+        <td width="10%">&nbsp;</td>
         <td width="80%" style="text-align: center">
-            <strong style="font-size: 10px;font-family: <?=$fontfamily;?>">INSTALASI FARMASI</strong><br>
             <span style="font-size:8px;font-family: <?=$fontfamily;?>">RSUD KABUPATEN KEDIRI</span>
         </td>
-        <td width="10%"></td>
+        <td width="10%">&nbsp;</td>
     </tr>
 </table>
 
-<table style="font-size: 12px;font-family: <?=$fontfamily;?>" cellpadding="0">
+<table border="0" width="100%" style="font-size: 11px;font-family: <?=$fontfamily;?>" cellpadding="0">
     <tr>
-        
-        <td><?=$model->penjualan->kode_penjualan;?></td>
-    </tr>
-    
-     <tr>
-        <td><?=date('d/m/Y');?></td>
-    </tr>
-    <tr>
-        <td><?=$model->penjualan->penjualanResep->pasien_nama.'/'.$model->penjualan->penjualanResep->pasien_id;?></td>
+        <td colspan="2"><?=$model->penjualan->penjualanResep->pasien_nama.'/'.$model->penjualan->penjualanResep->pasien_id;?></td>
     </tr>
     
     <tr>
-        <td><?=$is_racikan ? 'Racikan' : $model->stok->barang->nama_barang;?></td>
+        <td colspan="2"><?=$is_racikan ? 'Racikan' : $model->stok->barang->nama_barang;?></td>
     </tr>
      <tr>
-        <td></td>
+        <td width="50%">Jumlah : </td>
+        <td width="50%">ED : <?=date('d/m/Y');?></td>
     </tr>
     <tr>
-        <td><?=$model->signa1.' x sehari '.$model->signa2;?> .........................<br>............sebelum/sesudah/bersama makan</td>
+        <td width="50%">Pagi : </td>
+        <td width="50%">Sore : </td>
+    </tr>
+    <tr>
+        <td width="50%">Siang : </td>
+        <td width="50%">Malam : </td>
+    </tr>
+    <tr>
+        <td colspan="2"><?=$model->signa1.' x sehari '.$model->signa2;?> .........................<br>sebelum/sesudah/bersama makan</td>
     </tr>
 </table>
 
